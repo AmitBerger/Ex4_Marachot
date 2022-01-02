ef431346d2e119c7090d68dd3ad48d8f74a4ac71
AmitBerger <92735064+AmitBerger@users.noreply.github.com>
2022-01-02 13:33:50 +0200

    Update and rename priorityQueue.h to PQ.h

--- PQ.h
+++ PQ.h
@@ -1,14 +1,10 @@
-//
-// Created by PC on 12/29/2021.
-//
 #include "graph.h"
 #ifndef EX4_PRIORITYQUEUE_H
 #define EX4_PRIORITYQUEUE_H
 
 typedef struct PQNode {
-
     pNode data;
-    // Lower values indicate higher priority
+
     int priority;
 
     struct PQNode *next;
@@ -21,4 +17,4 @@ pqNode* pop(pqNode** head);
 void push(pqNode** head, pNode d, int p);
 int isEmpty(pqNode** head);
 
-#endif //EX4_PRIORITYQUEUE_H
+#endif 
